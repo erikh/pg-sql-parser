25ea42b4cc74042797b93f4947d5b91e34fb55bc
Erik Hollensbe <git@hollensbe.org>
2023-10-13 00:31:36 -0700

    more tokens, mostly quoting stuff
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/tokens/collection.rs
+++ src/tokens/collection.rs
@@ -15,7 +15,9 @@ use crate::make_token;
 // implementation for, which is why the definitions can be re-used here with format strings.
 //
 
-// whitespace tokens
+/*
+ * -- whitespace tokens --
+ */
 make_token!(Newline, r"\n\r");
 make_token!(NonNewlineSpace, r" \f\v\t");
 make_token!(Space, r"[{}{}]", Newline, NonNewlineSpace);
@@ -34,3 +36,68 @@ make_token!(
     Newline,
     SpecialWhitespace
 );
+
+/*
+ * -- quoting tokens --
+ */
+make_token!(Quote, "'");
+make_token!(QuoteContinue, "{}{}", WhitespaceWithNewline, Quote);
+// this is a special token used to determine when quoting runs into a comment and thus fails to
+// parse
+make_token!(QuoteContinueFail, "(?:{})*-?", Whitespace);
+
+/*
+ * -- bit strings --
+ */
+
+// bytes
+make_token!(XBStart, "[bB]{}", Quote);
+make_token!(XBInside, "[^']*");
+
+// hex
+make_token!(XHStart, "[xX]{}", Quote);
+make_token!(XHInside, "[^']*");
+
+// national (?) characters
+make_token!(XNStart, "[nN]{}", Quote);
+
+// Quoted string with escaping
+make_token!(XEStart, "[eE]{}", Quote);
+make_token!(XEInside, "[^\\']+");
+make_token!(XEEscape, "[\\][^0-7]");
+make_token!(XEOctEscape, "[\\][0-7]{1,3}");
+make_token!(XEHexEscape, "[\\]x[0-9a-fA-F]{1,2}");
+make_token!(XEUnicode, r"[\\](?:u[0-9a-fA-F]{4}|U[0-9a-fA-F]{8})");
+make_token!(
+    XEUnicodeFail,
+    r"[\\](?:u[0-9a-fA-F]{0,3}|U[0-9a-fA-F]{0,7})"
+);
+
+// Extended Quote
+make_token!(XQStart, "[{}]", Quote);
+make_token!(XQDouble, "[{}]{{2}}", Quote);
+make_token!(XQInside, "[^']+");
+
+// Dollar quoting
+make_token!(DolQStart, r"[A-Za-z\200-\377_]");
+make_token!(DolQContinue, r"[A-Za-z\200-\377_0-9]");
+make_token!(DolQDelimiter, r"\$(?:{}{}*)?\$", DolQStart, DolQContinue);
+make_token!(DolQFailed, r"\$(?:{}{}*", DolQStart, DolQContinue);
+make_token!(DolQInside, r"[^$]+");
+
+// Double quoting
+
+make_token!(DoubleQuote, "\"");
+make_token!(XDStart, "{}", DoubleQuote);
+make_token!(XDStop, "{}", DoubleQuote);
+make_token!(XDDouble, "{}{}", DoubleQuote, DoubleQuote);
+make_token!(XDInside, "[^\"]+");
+
+// Rules for Unicode Escapes
+
+// Quoted Identifier with Unicode Escapes
+make_token!(XUIStart, "[uU]&{}", DoubleQuote);
+// Quoted String with Unicode Escapes
+make_token!(XUSStart, "[uU]&{}", Quote);
+// Error Rule
+make_token!(XUFailed, "[uU]&");
